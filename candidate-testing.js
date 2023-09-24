6d084dea5fb10c16624dcf0e30d6f923caa88fcc
ngarner98 <exmachina091098@gmail.com>
2023-09-24 13:40:02 -0500

    Part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,10 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ', '(5 + 3)/2 * 10 = ? ',
+ "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 'What is the minimum crew size for the ISS? '];
+let correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', '3'];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -23,19 +24,26 @@ candidateName = input.question("Enter your name: ");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question + "\n")
+// candidateAnswer = input.question(question + "\n")
 
+for (i = 0; i < questions.length; i++) {
+  let answer = input.question(questions[i]);
+  candidateAnswers.push(answer);
+}
+// this.candidateAnswers = candidateAnswers;
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer = correctAnswer) {
-  console.log("Your answer is correct!");
-} else {
-  console.log("Your answer is incorrect...");
+// if (candidateAnswer.toLowerCase === correctAnswer.toLowerCase) {
+//   console.log("Your answer is correct!");
+// } else {
+//   console.log("Your answer is incorrect...");
+// }
+for (i = 0; i < correctAnswers.length; i++) {
+console.log(`For question ${[i+1]} you said ${candidateAnswers[i]} and the correct answer is ${correctAnswers[i]}.`)
 }
-
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
@@ -48,7 +56,8 @@ function runProgram() {
   // TODO 1.1c: Greet candidate using their name //
    console.log("Hello " + candidateName + " welcome to the quiz.");
   askQuestion();
-  gradeQuiz(this.candidateAnswers);
+  // gradeQuiz(this.candidateAnswers);
+  gradeQuiz(candidateAnswers);
 }
 
 // ----------- Don't write any code or change any code below this line ---------- //
